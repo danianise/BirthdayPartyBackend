b47f02765b1e42ce44aa09e5ab1a4eff96f83a3a
danianise <danianisehoey@gmail.com>
2022-10-10 22:16:27 -0400

    Redo

---
+++ birthday_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Decline, Response
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = ('name', 'guest_count', 'message')
+
+class DeclineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Decline
+        fields = ('name', 'decline_message')
