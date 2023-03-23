9ed179bc25a87134cb17eb453cbcb581e5bef620
danianise <danianisehoey@gmail.com>
2023-03-23 10:48:47 -0400

    Error fix for RSVP

--- birthday_app/models.py
+++ birthday_app/models.py
@@ -9,7 +9,7 @@ class Response(models.Model):
     message = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
-        return '{}, {} people in party'.format(self.name, self.guest_count)
+        return '{}, {} adults and {} kids in party'.format(self.name, self.adult_count, self.kids_count)
 
 class Decline(models.Model):
     name = models.CharField(max_length=100)
